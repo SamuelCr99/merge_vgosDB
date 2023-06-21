0099cb920b031324acbed12340adaab604c70b6a
Samuel Collier Ryder <samuel.collier.ryder@gmail.com>
2023-06-21 10:11:08 -0400

    Added data from german db
    Co-authored-by: FilipHerbertsson <FilipHerbertsson@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
 import netCDF4 as nc
 
-file_id = nc.Dataset('data/20APR01XA/Apriori/Antenna.nc')
+ds = nc.Dataset('data/20APR01XA/Apriori/Antenna.nc')
 
-print(file_id)
+# print(ds['AntennaStationList'][:])
+
+# for item in ds['AntennaStationList']:
+#     print(item)
+
+
+# for var in ds.variables:
+#     print(var)
+#     print(ds[var][::])
+
+# print('------')
+
+# for attr in ds.dimensions: 
+#     print(attr)
+
+# for dim in ds.dimensions.values():
+#     print(dim)
+
+# for var in ds.variables.values():
+#     print(var)
+
+# print(ds['Stub'][:].tobytes())
+
+for variable in ds.variables:
+    print(f'{variable}: {ds[variable][:].tobytes()}')
