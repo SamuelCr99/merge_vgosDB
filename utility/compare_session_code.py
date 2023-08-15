7117c100a3c2fe9e66baeb1f59b139f46f5d5315
Samuel Collier Ryder <samuel.collier.ryder@gmail.com>
2023-08-15 15:07:16 -0400

    Removed useless print
    Co-authored-by: FilipHerbertsson <FilipHerbertsson@users.noreply.github.com>

--- utility/compare_session_code.py
+++ utility/compare_session_code.py
@@ -15,7 +15,6 @@ def compare_session_code(merge_wrappers, secondary_wrappers):
             if "Session " in line: 
                 session_codes.append(line.strip("Session "))
                 break
-    print(session_codes)
     
     if len(set(session_codes)) != 1:
         raise Exception("You cannot merge vgosDB from different sessions")
